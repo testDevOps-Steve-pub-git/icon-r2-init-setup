604e3b5b28b11e0b49d10da6a0d8cb12df0b9ca4
Ankita Kulkarni & Shifeng Jin <ankitadk@ca.ibm.com>
2017-04-12 12:28:19 -0400

    while loop testing

--- scripts/exec_psql.sh
+++ scripts/exec_psql.sh
@@ -1,1 +1,7 @@
-psql -U admin -d compose -p $PORT -h $HOST  -f /tmp/scripts/1_Add_Roles.sql
+while [ 1 ]
+do 
+  sleep 5
+done
+# retrive credential from VCAP. 
+# save pass into a file ".pgpass"
+# psql -U admin -d compose -p $PORT -h $HOST  -f /tmp/scripts/1_Add_Roles.sql
