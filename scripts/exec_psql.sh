086b7b66f7743930f9de91d96d254ee7bce996a2
Ankita Kulkarni & Shifeng Jin <ankitadk@ca.ibm.com>
2017-04-12 14:10:23 -0400

    checking for VCAP

--- scripts/exec_psql.sh
+++ scripts/exec_psql.sh
@@ -7,4 +7,5 @@ echo "Running PSQL scripts"
 export PGPASSWORD=PUONXOHGXDBQCGFC 
 psql -U admin -d compose -p 22334 -h sl-us-dal-9-portal.6.dblayer.com --no-password -f /tmp/scripts/1_Add_Roles.sql
 
+env 
 echo "PSQL scripts ran"
