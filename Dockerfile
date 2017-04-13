5e65f52b6b99236b67533413875fa420a4f1649d
Shifeng Jin && Ankita Kulkarni <kulkani.ankita09@gmail.com>
2017-04-13 16:12:02 -0400

    psql scripting completed

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM postgres
 COPY ./* /tmp/scripts/
-COPY scripts/* /tmp/scripts/
-
 # Run PSQL scripts
 ENTRYPOINT /tmp/scripts/exec_psql.sh
--- scripts/exec_psql.sh
+++ scripts/exec_psql.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
-
 echo "Running PSQL scripts"
-# # retrive credential from VCAP. 
-# # save pass into a file ".pgpass"
 
-export PGPASSWORD=PUONXOHGXDBQCGFC 
-psql -U admin -d compose -p 22334 -h sl-us-dal-9-portal.6.dblayer.com --no-password -f /tmp/scripts/1_Add_Roles.sql
+echo "Retrieve credential from VCAP"
+URL=$(printenv VCAP_SERVICES_COMPOSE_FOR_POSTGRESQL_0_CREDENTIALS_URI)
 
-printenv VCAP_SERVICES_COMPOSE_FOR_POSTGRESQL_0_CREDENTIALS_URI
-echo "PSQL scripts ran"
+USER=$(echo $URL | cut -d ':' -f 2 | cut -d '/' -f 3)
+export PGPASSWORD=$(echo $URL | cut -d ':' -f 3 | cut -d '@' -f 1)
+HOST=$(echo $URL | cut -d ':' -f 3 | cut -d '@' -f 2)
+PORT=$(echo $URL | cut -d ':' -f 4 | cut -d '/' -f 1)
+DBNAME=$(echo $URL | cut -d ':' -f 4 | cut -d '/' -f 2)
+
+
+echo "Preparing for SQL scripting"
+runPSQL() {
+  psql -U $USER -d $DBNAME -p $PORT -h $HOST --no-password -f /tmp/scripts/$1
+}
+
+echo "Running Drop_DB.sql"
+runPSQL Drop_DB.sql
+
+echo "Running 1_Add_Roles.sql"
+runPSQL 1_Add_Roles.sql
+
+echo "Running 2_Create_Lookup_DB.sql"
+runPSQL 2_Create_Lookup_DB.sql
+
+echo "Running 3_Create_Submit_DB.sql"
+runPSQL 3_Create_Submit_DB.sql
+
+echo "Unzip 4_Geocode_Data.sql.zip and running 4_Geocode_Data.sql"
+tar xvf 4_Geocode_Data.sql.zip
+runPSQL 4_Geocode_Data.sql
+
+echo "Running 5_Lookup_Data.sql"
+runPSQL 5_Lookup_Data.sql
+
+
+echo "PSQL scripting completed"
