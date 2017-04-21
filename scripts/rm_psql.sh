4ae989248830ed5c5729ca57ebfaf4014d22d2a6
shifeng Jin <shifeng.jin@ontario.ca>
2017-04-21 11:36:06 -0400

    update container name

--- scripts/rm_psql.sh
+++ scripts/rm_psql.sh
@@ -2,15 +2,16 @@
 TIMEOUT=43200
 NOW=0
 INTERVAL=10
+PSQL_CONTAINER_NAME="${CONTAINER_NAME}_${BUILD_NUMBER}"
 while [  $NOW -le $TIMEOUT ]
 do
-    CONTAINER_STATUS=$(cf ic inspect -f '{{.State.Status}}' ${MY_CONTAINER_NAME})
+    CONTAINER_STATUS=$(cf ic inspect -f '{{.State.Status}}' ${PSQL_CONTAINER_NAME})
     if [ "$CONTAINER_STATUS" == "Shutdown" -o "$CONTAINER_STATUS" == "Running" -o "$CONTAINER_STATUS" == "Building" ]
     then
          if [ $CONTAINER_STATUS == "Shutdown" ]
          then
-             echo "Deleting ${MY_CONTAINER_NAME}"
-             cf ic rm ${MY_CONTAINER_NAME}
+             echo "Deleting ${PSQL_CONTAINER_NAME}"
+             cf ic rm ${PSQL_CONTAINER_NAME}
              exit 0
          fi
     else
