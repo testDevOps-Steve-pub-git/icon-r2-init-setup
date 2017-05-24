d9b4f8c968d2511486ac8678fef6be03fcb2e07f
shifeng Jin <shifeng.jin@ontario.ca>
2017-05-24 11:07:47 -0400

    R 2.33.1

---
+++ DeleteLookupData.sql
@@ -0,0 +1,10 @@
+-- clear all lookup data
+-- excluding geocode
+
+delete from agents_diseases;
+delete from trades_agents;
+delete from lots;
+delete from diseases;
+delete from agents;
+delete from trades;
+delete from schools_daycares;
